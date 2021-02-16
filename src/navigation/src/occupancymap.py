530e337514c66e17b2c86cbf7912b53da8adc297
htasnim <htasnim30@unm.edu>
2021-02-15 22:08:26 -0700

    Removed base code: probabilistic_road_map.py. Cleaned up occupancymap.py with consistent indentation.

--- src/navigation/src/occupancymap.py
+++ src/navigation/src/occupancymap.py
@@ -6,8 +6,7 @@ from PIL import Image
 ros_path = '/opt/ros/kinetic/lib/python2.7/dist-packages'
 
 if ros_path in sys.path:
-
-    sys.path.remove(ros_path)
+	sys.path.remove(ros_path)
 
 import cv2
 
@@ -15,25 +14,14 @@ sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 
 
 gridline  = np.ones((3000,3000))
-#for i in range(0,3000):
-#	for j in range(0,3000):
-#		gridline[i][j] = 255
-
 gridline = gridline * 255
-#im.show()
-
-#for i in range(0,100):
-#	cv2.circle(gridline,(1500, 1500), 1300+i, 0,2)
-
-#x = [4.3998, 4.4369, 4.3404]
-#y = [-0.2118, -0.7357, -1.2669] 
-
 pot_position_arr1 = []
+
 # replace nasa.world with file name
 my_file = open('NASA-MINDS1.world')
 
 for line in my_file:
-    # removes leading spaces ' ' and '<' in the line
+	# removes leading spaces ' ' and '<' in the line
     line = line.lstrip(' <')
     # gets the line starting with pose
     if line.startswith('model name=\'plant'):
@@ -43,29 +31,20 @@ for line in my_file:
         line = line.lstrip(' <')
         if line.startswith('pose'):
             pot_position_arr1.append(line.split('>')[1].split("<")[0])
-
 x = []
 y = []
-print(len(pot_position_arr1))
+
 for i in pot_position_arr1:
 	x.append(float(i.split()[0]))
 	y.append(float(i.split()[1]))
     
 
 for i in range(0,len(x)):
-	 #if x[i]>0.0:
-	#	 x_cen = round(1500 + x[i]*100)
-	 #else:
-	#	 x_cen = round(1500 - x[i]*100)
-	 #if y[i]>0.0:
-		# y_cen = round(1500 - y[i]*100)
-	 #else:
-		# y_cen = round(1500 + y[i]*100)
-	 x_cen = round(1500+x[i]*100)
-	 print(x_cen)
-	 y_cen = round(1500-y[i]*100)
-	 print(y_cen)
-	 cv2.circle(gridline,(x_cen, y_cen), 25, 0, -1)
+	x_cen = round(1500+x[i]*100)
+	print(x_cen)
+	y_cen = round(1500-y[i]*100)
+	print(y_cen)
+	cv2.circle(gridline,(x_cen, y_cen), 25, 0, -1)
 
 im = Image.fromarray(gridline)
 
